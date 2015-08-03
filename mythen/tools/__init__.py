d69592b10a700e32f3177c457510b44b319e9bc9
Roberto J. Homs Puron <rhoms@cells.es>
2015-08-03 11:03:54 +0200

    First commit.

---
+++ mythen/tools/__init__.py
@@ -0,0 +1,17 @@
+################################################################################
+#                                 Constants
+################################################################################
+class Const(object):
+    """
+    Class with the physical and mechanical constants.
+    """
+    c = 299792458           # light speed constant
+    h = 4.13566727e-15      # Planck constant
+    aSi = 5.43102088e-10    # Silicon lattice constant
+    R = 1000                # Radius of the Rowland circle
+    class S111(object):
+        h =
+        k =
+        l =
+        a =
+
