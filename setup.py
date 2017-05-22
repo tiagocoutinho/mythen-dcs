95db8232b64143917232ce29bce9507e935f7a34
Roberto J. Homs Puron <rhoms@cells.es>
2017-05-22 10:40:22 +0200

    Bump version 1.3.4-alpha to 1.3.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version = '1.3.4-alpha'
+__version = '1.3.4'
 
 setup(
     name='MythenDCS',
